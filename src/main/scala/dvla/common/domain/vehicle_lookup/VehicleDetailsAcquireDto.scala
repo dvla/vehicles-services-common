668536f87983ede0c1243593bd9afa63e984e4ac
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-08-26 15:17:55 +0100

    Add dto and respose for acquire

---
+++ src/main/scala/dvla/common/domain/vehicle_lookup/VehicleDetailsAcquireDto.scala
@@ -0,0 +1,6 @@
+package dvla.common.domain.vehicle_lookup
+
+class VehicleDetailsAcquireDto(registrationNumber: VehicleRegistrationMark,
+                               vehicleMake: String,
+                               vehicleModel: String,
+                               disposeFlag: Boolean)
---
+++ src/main/scala/dvla/common/domain/vehicle_lookup/VehicleLookupAcquireResponse.scala
@@ -0,0 +1,4 @@
+package dvla.common.domain.vehicle_lookup
+
+class VehicleLookupAcquireResponse (responseCode: Option[String],
+                                    vehicleDetailsDto: Option[VehicleDetailsAcquireDto])
